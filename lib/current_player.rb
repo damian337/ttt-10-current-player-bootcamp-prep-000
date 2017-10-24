9606b48f2689aade7b614fb71f35684edfa4216f
Damian Guillory <guillorywdamian@gmail.com>
2017-10-24 00:32:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
     counter = 0
     board.each do |index|
-        if index == "X" || "O" do
+        if index == "X" || "O" {
             counter +=1
-        end
+        }
     return counter
     end
 end

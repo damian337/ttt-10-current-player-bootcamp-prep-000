5776e8b9b91e749aba1771791162b920ef0f80be
Damian Guillory <guillorywdamian@gmail.com>
2017-10-24 00:42:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-    if turn_count(board).even?
-        return "X"
-    else
-        return "O"
-    end
+    if turn_count(board).even? "X" : "O"
 end
+
+input_type = gets =~ /hello/i ? "greeting" : "other"
